87c37cde1db172006cb2c4ec6918ba28365f2522
Manikandan37 <30549771+Manikandan37@users.noreply.github.com>
2017-08-05 16:52:52 +0530

    EN
    EVEN OR ODD

---
+++ EN.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int yen;
+clrscr();
+printf("\n ENTER THE NUMBER");
+scanf("%d",&yen);
+if(yen%2==0)
+printf("no is even");
+else
+printf("no is odd");
+getch();
+}
