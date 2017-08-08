da54bf8bd6052ff2ea9b116195365a43c7e6e3e4
Manikandan37 <30549771+Manikandan37@users.noreply.github.com>
2017-08-08 04:11:53 -0700

    largest.C
    largest number

---
+++ largest.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,c;
+    printf("Enter the a,b,c values: ");
+    scanf("%d%d%d",&a,&b,&c);
+    if(a>=b&&a>=c)
+    printf("%d is the largest number.", a);
+    else if(b>=c)
+    printf("%d is the largest number.",b);
+    else
+    printf("%d is the largest number.", c);
+    getch();
+}
