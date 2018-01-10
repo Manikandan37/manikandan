696fd980277d36e9f2dded5c77c0b4b1aae4e503
Manikandan37 <30549771+Manikandan37@users.noreply.github.com>
2018-01-10 15:59:29 +0530

    Update nn.C

--- nn.C
+++ nn.C
@@ -2,18 +2,13 @@
 #include<conio.h>
 void main()
 {
-    int n,i,sum=0;
-
-    printf("Enter a positive integer: ");
-    scanf("%d",&n);
-
-    for(i=1;i<=n;++i)
-    {
-	sum+=i;
-    }
-
-    printf("Sum=%d",sum);
-    getch();
-
-
+int n,i,sum=0;
+printf("Enter a positive integer: ");
+scanf("%d",&n);
+for(i=1;i<=n;++i)
+{
+sum+=i;
+}
+printf("Sum=%d",sum);
+getch();
 }
