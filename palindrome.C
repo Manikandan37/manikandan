2277b632a492fa739b443fa32838798ce4ac1cf6
Manikandan37 <30549771+Manikandan37@users.noreply.github.com>
2018-01-17 15:55:23 +0530

    palindrome.C
    palindrome or not

---
+++ palindrome.C
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+int n,rev=0,remain,orig;
+printf("Enter an integer: ");
+scanf("%d",&n);
+orig=n;
+while(n!=0 )
+{
+ remain=n%10;
+ rev=rev*10+remain;
+ n/= 10;
+}
+if(orig==rev)
+printf("%d is a palindrome.",orig);
+else
+printf("%d is not a palindrome.",orig);
+getch();
+}
