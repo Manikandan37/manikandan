3b72712cfe0b10a437596dfbf89f639e748e0c57
Manikandan37 <30549771+Manikandan37@users.noreply.github.com>
2018-01-22 16:42:04 +0530

    arm.C
    armstrong number

---
+++ arm.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,a,remain,result=0;
+printf("Enter a three digit integer: ");
+scanf("%d",&n);
+a=n;
+while(a!=0)
+{
+remain=a%10;
+result+=remain*remain*remain;
+a/=10;
+}
+if(result==n)
+printf("%d yes ",n);
+else
+printf("%d no.",n);
+getch();
+}
