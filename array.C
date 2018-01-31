c5484904285b4c7309d7453334939240f0176fa2
Manikandan37 <30549771+Manikandan37@users.noreply.github.com>
2018-01-31 16:42:16 +0530

    array.C
    find sum of 1st two numbers

--- array.C
+++ array.C
@@ -2,14 +2,14 @@
 #include<conio.h>
 void main()
 {
-int n,i,sum=0;
+int n,i,s=0;
 clrscr();
 printf("Enter the Range:");
 scanf("%d",&n);
 for(i=1;i<=n;i++)
 {
-sum=sum+i;
+s=s+i;
 }
-printf("The sum of Numbers between the range 0 to %d is: %d",n,sum);
+printf("The sum of Numbers between the range 0 to %d is: %d",n,s);
 getch();
 }
