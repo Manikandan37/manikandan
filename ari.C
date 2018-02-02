3f421ff6f99acc68226ef4d5cd704f3d52035635
Manikandan37 <30549771+Manikandan37@users.noreply.github.com>
2018-02-02 15:55:34 +0530

    ari.C
    find the sum of arithmetic progression

--- ari.C
+++ ari.C
@@ -1,26 +1,20 @@
-#include <stdio.h>
-#include <stdlib.h>
- 
-int main() {
-    int first, diff, terms, value, sum=0, i;
- 
-    printf("Enter the number of terms in AP series\n");
-    scanf("%d", &terms);
- 
-    printf("Enter first term and common difference of AP series\n");
-    scanf("%d %d", &first, &diff);
- 
-    /* print the series and add all elements to sum */
-    value = first;
-    printf("AP SERIES\n");
-    for(i = 0; i < terms; i++) {
-        printf("%d ", value);
-        sum += value;
-        value = value + diff;
-    }
- 
-    printf("\nSum of the AP series till %d terms is %d\n", terms, sum);
- 
-    getch();
- return 0;
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,n,value,sum=0,i;
+printf("Enter the number of terms in AP series\n");
+scanf("%d",&n);
+printf("Enter first term and common difference of AP series\n");
+scanf("%d%d",&a,&b);
+value=a;
+printf("AP SERIES\n");
+for(i=0;i<n;i++)
+{
+printf("%d",value);
+sum+=value;
+value=value+b;
+}
+printf("\nSum of the AP series till %d terms is %d\n",n,sum);
+getch();
 }
