2c30a6706e223d905178e4862df2e54b8b766a9f
Manikandan37 <30549771+Manikandan37@users.noreply.github.com>
2018-02-02 16:19:56 +0530

    arrmin.C
    find minimum  element using array

---
+++ arrmin.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int array[100],min,size,i;
+clrscr();
+printf("Enter the number of elements in array\n");
+scanf("%d",&size);
+printf("Enter %d integers\n",size);
+for (i=0;i<size;i++)
+scanf("%d",&array[i]);
+min=array[0];
+for(i=1;i<size;i++)
+{
+if(array[i]<min)
+{
+min=array[i];
+}
+}
+printf("Minimum element value is%d",min);
+getch();
+}
