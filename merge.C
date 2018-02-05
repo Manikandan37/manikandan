a20b7ca6b866996480427506b2e078d871486387
Manikandan37 <30549771+Manikandan37@users.noreply.github.com>
2018-02-05 16:34:33 +0530

    merge.C
    merge sort

---
+++ merge.C
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,b[10],i,num,j,k,temp;
+clrscr();
+printf("enter the limit");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&b[i]);
+}
+for(j=1;j<n;j++)
+{
+for(k=j+1;k<n;k++)
+{
+if(b[j]>b[k])
+{
+temp=b[j];
+b[j]=b[k];
+b[k]=temp;
+printf("%d",b[j]);
+}
+}
+n=((n+1)/2)-1;
+printf("%d",n);
+}
+getch();
+}
