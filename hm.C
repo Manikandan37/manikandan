b6a99277ba8133145162ddcc025c21d2eb7c559c
Manikandan37 <30549771+Manikandan37@users.noreply.github.com>
2018-02-05 16:16:13 +0530

    hm.C
    subtract hours and minutes

---
+++ hm.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int h1,h2,m1,m2,c,d;
+clrscr();
+printf(" h1 and m1");
+scanf("%d%d",&h1,&m1);
+printf(" h1 and m1");
+scanf("%d%d",&h2,&m2);
+c=h1-h2;
+d=m1-m2;
+printf("%d%d",c,d);
+getch();
+}
