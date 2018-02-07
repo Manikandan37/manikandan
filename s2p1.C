cc82aa6e8eab93753b79c5da74cc436f53cf0aea
Manikandan37 <30549771+Manikandan37@users.noreply.github.com>
2018-02-07 16:56:00 +0530

    power of number

--- s2p1.C
+++ s2p1.C
@@ -2,14 +2,14 @@
 #include <conio.h>
 void  main()
 {
- int ba,ex,i,result=1;
+ int ba,ex,i,res=1;
  clrscr();
  printf("Enter base and exponent \n");
  scanf("%d%d",&ba,&ex);
  for(i=0;i<ex;i++)
  {
- result=result*ba;
+ res=res*ba;
  }
-  printf("%d^%d=%d",ba,ex,result);
+  printf("%d^%d=%d",ba,ex,res);
   getch();
  }
