29b53624d5aead2bf92548dbeb376b091faba4fb
Manikandan37 <30549771+Manikandan37@users.noreply.github.com>
2018-02-09 15:58:09 +0530

    sum of arithmetic progression

--- s3p1.C
+++ s3p1.C
@@ -3,6 +3,7 @@
 void main()
 {
 int a,b,n,value,sum=0,i;
+clrscr();
 printf("Enter the number of terms in AP series\n");
 scanf("%d",&n);
 printf("Enter first term and common difference of AP series\n");
