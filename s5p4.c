4e5bd6605542d1c5f817771e83dc0812e470ed95
Manikandan37 <30549771+Manikandan37@users.noreply.github.com>
2018-02-21 15:48:53 +0530

    check the range from 1 to 9

---
+++ s5p4.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+printf("\n Enter the number\t");
+scanf("%d",&a);
+if(a>0&&a<10)
+printf("yes");
+else
+printf("no");
+getch();
+}
