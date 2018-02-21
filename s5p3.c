df46b4203feed0aff2c1d7b1c779bd142e1cf833
Manikandan37 <30549771+Manikandan37@users.noreply.github.com>
2018-02-21 15:41:17 +0530

    concatenation of two srings

---
+++ s5p3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char s1[20],s2[20];
+int i;
+clrscr();
+printf("\n enter the two string\t");
+scanf("%s%s",s1,s2);
+printf("\n %s%s",s1,s2);
+getch();
+}
