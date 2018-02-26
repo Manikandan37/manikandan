11d9d4c41eb585b1f3fa4ced53c8b13011b8632d
Manikandan37 <30549771+Manikandan37@users.noreply.github.com>
2018-02-26 16:19:10 +0530

    print the string 0 to 9

---
+++ s6p2.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+printf("\n Enter the number");
+scanf("%d",&a);
+if(a==0)
+printf("zero");
+else if(a==1)
+printf("one");
+else if(a==2)
+printf("two");
+else if(a==3)
+printf("three");
+else if(a==4)
+printf("four");
+else if(a==5)
+printf("five");
+else if(a==6)
+printf("six");
+else if(a==7)
+printf("seven");
+else if(a==8)
+printf("eight");
+else if(a==9)
+printf("nine");
+else
+printf("number is more than one");
+getch();
+}
