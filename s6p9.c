744c43e223d0816e8f4942a9b5ad86d157942cdb
Manikandan37 <30549771+Manikandan37@users.noreply.github.com>
2018-02-28 16:17:46 +0530

    maximum element

---
+++ s6p9.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[25],i,m=0;
+clrscr();
+for(i=0;i<=10;i++)
+{
+printf("enter the value");
+  scanf("%d",&a[i]);
+}
+  for(i=0;i<=10;i++)
+  {
+if(m<a[i])
+{
+m=a[i];
+}
+  }
+  printf("maximum element is %d",m);
+  getch();
+}
