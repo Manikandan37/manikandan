acd40affe41d4e29766eceabab4d28cf19d5bdb3
Manikandan37 <30549771+Manikandan37@users.noreply.github.com>
2018-03-02 16:40:25 +0530

    print sum of number is odd

---
+++ s7p4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+clrscr();
+printf("\n Enter the 2 numbers");
+scanf("%d%d",&a,&b);
+c=a+b;
+if(c%2==0)
+{
+printf("even");
+}
+else
+{
+printf("odd");
+}
+getch();
+}
