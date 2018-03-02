c518f224668bdd5ff3f80091987bec16b9a70963
Manikandan37 <30549771+Manikandan37@users.noreply.github.com>
2018-03-02 16:57:51 +0530

    multiple of 7 or not

---
+++ s7p8.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n;
+    clrscr();
+    printf("\n Enter the number");
+    scanf("%d",&n);
+    if(n%7==0)
+    printf("yes");
+    else
+    printf("no");
+    getch();
+}
