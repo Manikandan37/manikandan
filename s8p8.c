387ba2b4632f00955e465516faa6c9373079633c
Manikandan37 <30549771+Manikandan37@users.noreply.github.com>
2018-03-09 16:36:53 +0530

    multiple of 13 or not

---
+++ s8p8.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+  int num,i;
+  clrscr();
+  printf("Enter the number");
+  scanf("%d",&num);
+  if(num%13==0)
+  {
+    printf("YES");
+  }
+  else
+  {
+    printf("NO");
+    }
+    
+}
