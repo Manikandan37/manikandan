1610784a4d068860113946a0dbb371d07402e91b
Manikandan37 <30549771+Manikandan37@users.noreply.github.com>
2018-03-09 16:01:41 +0530

    float into integer

---
+++ s8p4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+float x;
+int a;
+clrscr();
+printf("enter the number\n");
+scanf("%f",&x);
+a=(int)(x+0.5);
+printf("Value Of a:%d",a);
+getch();
+}
