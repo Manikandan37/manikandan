3132a21a949272dc2f8dcdbcaa6c567b4f3ec74f
Manikandan37 <30549771+Manikandan37@users.noreply.github.com>
2018-03-09 16:47:54 +0530

    print odd number

---
+++ s8p10.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include<conio.h>
+int main(void)
+{
+int n,t,rem=0,i;
+printf("Enter the number");
+scanf("%d",&n);
+while(n!=0)
+{
+t=n%10;
+rem=(rem*10)+t;
+n=n/10;
+}
+while(rem!=0)
+{
+i=rem%10;
+rem=rem/10;
+if(i%2!=0)
+{
+printf("%d\n",i);
+}
+}
+getch();
+return 0;
+}
