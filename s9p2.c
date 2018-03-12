736f147c2ca28c1191110ffea809d7b9cb7d0b49
Manikandan37 <30549771+Manikandan37@users.noreply.github.com>
2018-03-12 16:03:01 +0530

    find area

---
+++ s9p2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+ float l,b,area;
+ printf("enter the l and b values");
+ scanf("%f %f",&l,&b);
+ area=l*b;
+ printf("%.5f",area);
+ getch();
+}
