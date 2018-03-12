d7ec8fe4489a6e39ca0a4d0c118eeab7bf407245
Manikandan37 <30549771+Manikandan37@users.noreply.github.com>
2018-03-12 16:11:15 +0530

    Create s9p3.c

---
+++ s9p3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main() 
+{	
+   int a,b,i;
+   char c;
+   for(i=0;i<4;i++)
+   {
+       scanf("%d %c %d",&a,&c,&b);
+       if(i%2==0)
+       {
+           printf("%d\n",(a/b));
+       }
+       else
+       {
+           printf("%d\n",(a%b));
+       }
+   }
+	getch();
+}
