20049bd927f3053251f6769a3ad8c9a88bed70fc
Manikandan37 <30549771+Manikandan37@users.noreply.github.com>
2018-03-14 16:49:43 +0530

    convert celsius into kelvin

---
+++ s10p3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+    int c,k;
+    clrscr();
+    printf("\n enter the number");
+    scanf("%d",&c);
+    k=c+273;
+    printf("%d",k);
+    getch();;
+}
