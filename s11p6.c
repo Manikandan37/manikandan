8c5c38389aa983987eecfa461eda82bb591d687e
Manikandan37 <30549771+Manikandan37@users.noreply.github.com>
2018-03-19 16:51:09 +0530

    character of given number

---
+++ s11p6.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a=64,n;
+    clrscr();
+    printf("Enter the number:\n");
+    scanf("%d",&n);
+    printf("%c\n",a+n);
+    getch();
+}
