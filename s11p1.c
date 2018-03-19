c01611c4ed66304f3e1df2c3f6df8ac9f2602b13
Manikandan37 <30549771+Manikandan37@users.noreply.github.com>
2018-03-19 16:00:48 +0530

    last n character

---
+++ s11p1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main()
+{
+	int n,l,p,i;
+  clrscr();
+	char s[100];
+  printf("\n Enter the number");
+  scanf("%s",s);
+  printf("\n enter the number");
+	scanf("%d",&n);
+	for(i=p;i<l;i++)
+	{
+		printf("%c",s[i]);
+	}
+	getch();	
+}
