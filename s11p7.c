2f996ed78f54152f2023316f2b9cfcf20e515f1d
Manikandan37 <30549771+Manikandan37@users.noreply.github.com>
2018-03-21 15:53:22 +0530

    a*b/c

---
+++ s11p7.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h> 
+int main()
+{
+  int a,b,c;
+  clrscr();
+  printf("\n enter the a,b,c values");
+  scanf("%d %d %d",&a,&b,&c);
+  printf("%d",(a*b)/c);
+  getch();
+}
