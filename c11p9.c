b6ec52427f617360e493cd22d2c050636d69f9e3
Manikandan37 <30549771+Manikandan37@users.noreply.github.com>
2018-03-26 16:22:28 +0530

    minimum number

---
+++ c11p9.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+    int a[10],min=0,i,n;
+    min=a[0];
+    clrscr();
+    printf("\n the numbers are:\n"):
+    for(i=0;i<10;i++)
+    {
+    scanf("%d",&a[i]);
+    }
+    for(i=1;i<10;i++)
+    {
+    if(a[i]<min)
+    min=a[i];
+    }
+    printf("%d",min);
+getch();
+}
