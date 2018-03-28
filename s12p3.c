d7c250f1c9aa00ec022764a0951da979378a0274
Manikandan37 <30549771+Manikandan37@users.noreply.github.com>
2018-03-28 15:52:57 +0530

    print repetition of k

---
+++ s12p3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+   int a[10],n,k,count=0,i;
+   printf("\n enter the n and k values");
+   scanf("%d %d",&n,&k);
+   for(i=0;i<n;i++)
+   {
+   scanf("%d",&a[i]);
+   }
+   for(i=0;i<n;i++)
+   {
+       if(a[i]==k)
+       count++;
+   }
+   printf("%d",count);
+   getch();
+}
+
